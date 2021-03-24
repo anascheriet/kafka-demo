b21bc37616041f98572242423207ecf6a5d2dd0e
Anas CHERIET <anascheriet@hotmail.com>
2021-03-24 16:32:19 +0100

    Produce data to consumer cli

--- src/main/java/com/acheriet/kafka/demo/ProducerDemo.java
+++ src/main/java/com/acheriet/kafka/demo/ProducerDemo.java
@@ -1,8 +1,29 @@
 package com.acheriet.kafka.demo;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
 public class ProducerDemo {
 
     public static void main(String[] args) {
-        System.out.println("hello world");
+        // create producer properties
+        String bootstrapServers = "127.0.0.1:9092";
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        // create the data to send (producer record)
+        ProducerRecord<String, String> record = new ProducerRecord<String, String>("first_topic", "can you see me");
+        // send data
+        producer.send(record);
+        producer.flush();
     }
 }
